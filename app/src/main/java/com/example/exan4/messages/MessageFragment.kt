c9891492d507097c80e0be386d92c70ad454fd60
lmelikishvili <levanimelikishvili@gmail.com>
2023-12-07 01:26:41 +0400

    base_setup

--- app/src/main/java/com/example/exan4/messages/MessageFragment.kt
+++ app/src/main/java/com/example/exan4/messages/MessageFragment.kt
@@ -6,10 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.exan4.BaseFragment
 import com.example.exan4.R
 import com.example.exan4.databinding.FragmentMessageBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MessageFragment : BaseFragment<FragmentMessageBinding>(FragmentMessageBinding::inflate){
     private val viewModel: MessageViewModel by viewModels()
@@ -23,7 +28,8 @@ class MessageFragment : BaseFragment<FragmentMessageBinding>(FragmentMessageBind
             messageRecyclerView.setHasFixedSize(true)
             messageRecyclerView.adapter = adapter
         }
-        adapter.submitList(viewModel.messages)
+        viewModel.addMessage()
+
     }
 
     override fun setupListeners() {
@@ -31,6 +37,13 @@ class MessageFragment : BaseFragment<FragmentMessageBinding>(FragmentMessageBind
     }
 
     override fun bindData() {
+        viewLifecycleOwner.lifecycleScope.launch{
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.messageFlow.collect{
+                    adapter.submitList(it)
+                }
+            }
+        }
 
     }
 
