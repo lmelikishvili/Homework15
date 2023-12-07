d5db0f27bf7f8884a576ac81d6f5312544e75e6d
lmelikishvili <levanimelikishvili@gmail.com>
2023-12-07 17:59:11 +0400

    before_api_remake

--- app/src/main/java/com/example/exan4/messages/Message.kt
+++ app/src/main/java/com/example/exan4/messages/Message.kt
@@ -1,12 +1,21 @@
 package com.example.exan4.messages
 
+import com.squareup.moshi.Json
+
 data class Message(
+    @Json(name = "id")
     val id: Int,
+    @Json(name = "owner")
     val owner: String,
+
     val lastMessage: String,
+
     val lastActive: String,
+
     val unreadMessages: Int,
+
     val isTyping: Boolean,
+
     val lastMessageType: LastMessageType
 ){
     enum class LastMessageType{
@@ -15,3 +24,8 @@ data class Message(
         FILE
     }
 }
+
+data class MessageResponse(
+    @Json(name = "results")
+    val results: List<Message>
+)
--- app/src/main/java/com/example/exan4/messages/MessageViewModel.kt
+++ app/src/main/java/com/example/exan4/messages/MessageViewModel.kt
@@ -1,22 +1,50 @@
 package com.example.exan4.messages
 
-import android.support.v4.os.IResultReceiver._Parcel
+import android.util.Log
+import android.util.Log.d
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Response
 
 class MessageViewModel: ViewModel() {
     private val _messageFlow = MutableStateFlow<List<Message>>(emptyList())
     val messageFlow: StateFlow<List<Message>> = _messageFlow
+    val client = ApiMessage.apiService.fetchMessages("744fa574-a483-43f6-a1d7-c65c87b5d9b2")
 
 
     fun addMessage(){
-        viewModelScope.launch {
-            _messageFlow.value = messageFlow.value.toMutableList().also {
-                it.add(Message(1,"Meliqa Meliqa", "gamarjoja rafer xar?", "4:30",2,true,Message.LastMessageType.TEXT))
+        client.enqueue(object : retrofit2.Callback<MessageResponse>{
+            override fun onResponse(
+                call: Call<MessageResponse>,
+                response: Response<MessageResponse>
+            ) {
+                if (response.isSuccessful){
+                    response.body()
+                    d("messageBody", " " + response.body())
+                    println("gazaaaaaaaaaaaaaaaaaa")
+                }
+                else{
+                    d("messageBodyNotGood", " " + response.body())
+                }
             }
-        }
+
+            override fun onFailure(call: Call<MessageResponse>, t: Throwable) {
+
+                println("Trakanaaaaaaa")
+                Log.e("falbalalaaaa", " " + t.message)
+            }
+        })
+
+
+
+
+
+//        viewModelScope.launch {
+//            _messageFlow.value = messageFlow.value.toMutableList().also {
+//                it.add(Message(1,"Meliqa Meliqa", "gamarjoja rafer xar?", "4:30",2,true,Message.LastMessageType.TEXT))
+//            }
+//        }
     }
 }
